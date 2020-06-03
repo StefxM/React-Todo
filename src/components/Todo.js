25dd8a22ad2790882a062eeb483a6862e5fbbb4e
stephanie mairena <stefyem7711@gmail.com>
2020-06-03 01:26:49 -0400

    setup

---
+++ src/components/Todo.js
@@ -0,0 +1,35 @@
+//- `<Todo />` is a component that takes in the `todo` data and displays the task to the screen.
+
+[
+    {
+      task: 'Organize Garage', // title
+      id: 1528817077286, //time stamp - `Date.now()`
+      completed: false//completed toggle
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    },
+    {
+        task: 'Wash Dishes',
+        id: 1528817084361,
+        completed: false
+    },
+    {
+        task: 'Laundry',
+        id: 1528817084364,
+        completed: false
+    },
+    {
+        task: 'Meal Prep',
+        id: 1528817084366,
+        completed: false
+    },
+    {
+        task: 'Set up App',
+        id: 1528817084369,
+        completed: false
+    }
+
+  ];
